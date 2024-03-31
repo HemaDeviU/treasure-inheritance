0c4f13058d3c298df72dc8d6bc1a483938eb4400
EL <135946602+HemaDeviU@users.noreply.github.com>
2024-03-31 11:56:12 +0530

    fixed the bug and added comments

--- src/treasure.sol
+++ src/treasure.sol
@@ -1,13 +1,14 @@
 //SPDX-License-Identifier:MIT
 pragma solidity 0.8.24;
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-
+//@title Inheritance contract
 contract Treasure is ReentrancyGuard {
+//state variables
     address payable private owner;
     address payable private heir;
     uint256 immutable public interval = 4 weeks;
     uint256 public lastWithdrawalTime;
-
+//errors
     error treasure__EnterValidAddress();
     error treasure__OnlyOwnerpermitted();
     error treasure__OnlyHeirPermitted();
@@ -16,12 +17,15 @@ contract Treasure is ReentrancyGuard {
     error treasure__Insufficientbalance();
     error treasure__YouCantBeYourHeir();
 
+//events
     event WithdrawalComplete(uint256 indexed amt, address indexed owner);
     event OwnershipUpdated(address indexed owner, address indexed heir);
-    event DepositReceived(address indexed sender, uint256 indexed despositAmount);
 
+//@dev assigns initial owner and heir for the contract and updates withdrawaltime to now
+//@param _owner : address of the owner
+//@param _heir : address of the heir
     constructor(address _owner, address _heir){
-        if(_owner == address(0) || _heir == address(0)){
+        if(_owner == address(0) || _heir == address(0)){ //checks for zero address
             revert treasure__EnterValidAddress();
         }
     owner = payable(_owner);
@@ -29,10 +33,10 @@ contract Treasure is ReentrancyGuard {
     lastWithdrawalTime = block.timestamp;
     }
     receive() external payable {
-        emit DepositReceived(msg.sender, msg.value);
     }
     fallback() external payable {}
 
+//modifiers
     modifier onlyOwner() {
         if(msg.sender != owner)
         {
@@ -47,12 +51,16 @@ contract Treasure is ReentrancyGuard {
         _;
     }
 
-    function withdraw(uint256 amount) external onlyOwner nonReentrant {
-        if(amount > address(this).balance)
+//functions
+//@dev allow withdrawals by owner if 'now' is between lastwithdrawaltime and interval. Transfer the amount to owner and set lastwithdrawaltime.
+//@param amount: amount to withdraw from the contract.
+//notice Call this function to withdraw amount from the contract balance. Specify the amount to withdraw. 0 withdrawals are allowed.
+    function withdraw(uint256 amount) external onlyOwner nonReentrant { //nonreentrant to avoid change of state
+        if(amount > address(this).balance) //security check
         {
             revert treasure__Insufficientbalance();
         }
-        if(block.timestamp < lastWithdrawalTime + interval)
+        if(block.timestamp >= lastWithdrawalTime + interval) //withdrawal allowed before interval expiry
         {
             revert treasure__Intervaltimeexceded();
         }
@@ -66,6 +74,11 @@ contract Treasure is ReentrancyGuard {
         require(success, "Failed to withdraw");
         }
     }
+
+//@dev called by heir to designate oneself as owner and assigns unique heir. 
+//@param _newHeir: address of the newheir to be designated as heir.
+//@notice The heir can call this function to become the owner, but only after the withdrawal interval has passed.
+
     function designateHeir(address _newHeir) external onlyHeir {
         if(block.timestamp < lastWithdrawalTime + interval)
         {
@@ -81,6 +94,7 @@ contract Treasure is ReentrancyGuard {
          heir = payable(_newHeir);
          
     }
+//getter functions
     function getBalance() public view returns (uint256 bal)
     {
         return address(this).balance;
